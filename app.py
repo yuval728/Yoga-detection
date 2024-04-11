009fb3f19ec70a5fb6be98ea4f55b14dd9a0936b
Yuval728 <yuvalmehta.728@gmail.com>
2024-04-11 11:38:13 +0530

    app changed

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask, render_template, Response
 import cv2
 import numpy as np
 import time
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
-app = Flask(__name__)
-model = load_model('yoga_model.keras')
+model = load_model('model.keras')
 classes = ['downdog', 'goddess', 'plank', 'tree', 'warrior2']
 
 def gen_frames():
@@ -16,37 +14,31 @@ def gen_frames():
         if not success:
             break
         else:
-            frame = cv2.flip(frame, 1)
-            resized_frame = cv2.resize(frame, (300, 300))
-            img_array = image.img_to_array(resized_frame)
-            img_array = np.expand_dims(img_array, axis=0)
-            img_array = img_array / 255.0
-            
-            prediction = model.predict(img_array)
-            label = classes[np.argmax(prediction)]
-            print(label, np.max(prediction))
-            
-            if np.max(prediction)<0.7:
-                print('Incorrect Pose')
-                cv2.putText(frame, f'Pose: {label} Incorrect', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-            else:
-                print('Correct Pose')
-                cv2.putText(frame, f'Pose: {label} Correct', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            # k=cv2.waitKey(1)
+            # cv2.imshow('frame', frame)
+            # if k%256 == 27:
+            #     # ESC pressed
+            #     print("Escape hit, closing...")
+            #     break
+            # if k%256 == 32:
+            #     # SPACE pressed
                 
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame_bytes = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
-            time.sleep(0.1)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/video_feed')
-def video_feed():
-    print('video_feed')
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
+                frame = cv2.flip(frame, 1)
+                resized_frame = cv2.resize(frame, (300, 300))
+                img_array = image.img_to_array(resized_frame)
+                img_array = np.expand_dims(img_array, axis=0)
+                img_array = img_array / 255.0
+                
+                prediction = model.predict(img_array)
+                label = classes[np.argmax(prediction)]
+                print(label, np.max(prediction))
+                
+                if np.max(prediction)<0.7:
+                    print('Incorrect Pose')
+                    # cv2.putText(frame, f'Pose: {label} Incorrect', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                else:
+                    print('Correct Pose')
+                    # cv2.putText(frame, f'Pose: {label} Correct', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                
 if __name__ == '__main__':
-    app.run(debug=False)
+    gen_frames()
